4be3e38a17e4311c0c3f4cefff7f113b75156511
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-03-05 23:59:18 +0200

    disable scroll while menu is opened & darken bg

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 // Burger menu
 const header = document.querySelector('.header');
-const menuButton = header.querySelector('.menu-button');
-const navList = header.querySelector('.nav-list');
+      menuButton = header.querySelector('.menu-button');
+      navList = header.querySelector('.nav-list');
+      darkenBG = document.querySelector('.darken-bg-menu');
 
 menuButton.addEventListener('click', () => {
     menuButton.classList.toggle('active');
     navList.classList.toggle('active');
+    document.body.classList.toggle('disable-scroll');
+    darkenBG.classList.toggle('show');
 });
 
 window.addEventListener('scroll', () => {
