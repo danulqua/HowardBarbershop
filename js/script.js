d8096715e0ea26e56e209b362ad40c34da3000fa
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-03-05 16:06:42 +0200

    section 'about us' has been added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 // Burger menu
-const menuButton = document.querySelector('.menu-button');
-const navList = document.querySelector('.nav-list');
+const header = document.querySelector('.header');
+const menuButton = header.querySelector('.menu-button');
+const navList = header.querySelector('.nav-list');
 
 menuButton.addEventListener('click', () => {
     menuButton.classList.toggle('active');
     navList.classList.toggle('active');
+});
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 700) {
+        header.classList.remove('transparent');
+    } else {
+        header.classList.add('transparent');
+    }
 });
