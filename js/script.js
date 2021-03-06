3f7d9203b1b316570966bec6d48d66a31c809d05
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-03-06 10:35:36 +0200

    slider has been added

--- js/script.js
+++ js/script.js
@@ -18,3 +18,33 @@ window.addEventListener('scroll', () => {
         header.classList.add('transparent');
     }
 });
+
+// Slider
+const sliderBlock = document.querySelector('.slider');
+      slidesArray = sliderBlock.querySelectorAll('.slider-item');
+      btnSlideLeft = sliderBlock.querySelector('#slide-left');
+      btnSlideRight = sliderBlock.querySelector('#slide-right');
+
+let currentItem = 0;
+
+const showSlide = (array, index) => {
+    array.forEach((slide, i) => {
+        slide.classList.remove('show');
+        slide.classList.add('hide');
+        
+        if (i == index) {
+            slide.classList.add('show');
+            slide.classList.remove('hide');
+        }
+    });
+}
+
+btnSlideRight.addEventListener('click', () => {
+    currentItem = (currentItem + 1) < slidesArray.length ? ++currentItem : 0;
+    showSlide(slidesArray, currentItem);
+});
+
+btnSlideLeft.addEventListener('click', () => {
+    currentItem = (currentItem - 1) > 0 ? --currentItem : slidesArray.length - 1;
+    showSlide(slidesArray, currentItem);
+});
